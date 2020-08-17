90f5eb9b0e549c9259484b64b90005708298b0cf
Luke Edwards <luke.edwards05@gmail.com>
2020-08-17 11:52:05 -0700

    fix(types): declare module per entry;
    - see https://github.com/lukeed/klona/issues/21

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
-export function dequal(foo: any, bar: any): boolean;
+declare module 'dequal' {
+	export function dequal(foo: any, bar: any): boolean;
+}
+
+declare module 'dequal/lite' {
+	export function dequal(foo: any, bar: any): boolean;
+}
